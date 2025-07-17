b5e3599473f91c401d054ad9625bdbbef0807fc9
Elviskiprono <biggeelvo831@gmail.com>
2025-07-17 22:15:43 +0300

    defined  Order model with total and created_at for sales tracking

---
+++ backend/app/models/order.py
@@ -0,0 +1,11 @@
+from ..extensions import db
+from datetime import datetime
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    total = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
